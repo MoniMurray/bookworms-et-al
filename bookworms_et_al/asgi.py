2515bed841432789e48d84529014c199322c3d09
Monica Murray <Monica.rickerby@gmail.com>
2023-06-04 09:27:12 +0000

    Add Initial commit

---
+++ bookworms_et_al/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bookworms_et_al project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookworms_et_al.settings')
+
+application = get_asgi_application()
---
+++ bookworms_et_al/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bookworms_et_al project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookworms_et_al.settings')
+
+application = get_wsgi_application()
