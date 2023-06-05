eceea21e09d5cd7bf076966daf6318b16c013f26
Monica Murray <Monica.rickerby@gmail.com>
2023-06-05 20:45:20 +0000

    Build homepage grid layout

--- bookworms_et_al/settings.py
+++ bookworms_et_al/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
 
+    # apps
+    'home',
 
 ]
 
@@ -65,7 +67,10 @@ ROOT_URLCONF = 'bookworms_et_al.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -151,6 +156,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
