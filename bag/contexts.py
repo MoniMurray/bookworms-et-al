45ec1ba6b98a511e8e123315b4c3bb10691b9c7c
Monica Murray <Monica.rickerby@gmail.com>
2023-06-10 21:05:47 +0000

    Add to context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,3 +1,8 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def bag_contents(request):
     """
@@ -5,8 +10,55 @@ def bag_contents(request):
     called 'context', it's purpose is to make the dictionary available
     to all templates accross the entire application
     """
+    bag_items = []
+    subtotal = 0
+    product_count = 0
+    bag = request.session.get('bag', {})
 
-    context = {}
+    # iterate through all the items in the shopping bag, and along the way tally up the
+    # total cost and product count and add the products and their data to the bag items list.
+
+    for item_id, quantity in bag.items():
+        # item data will be an int
+        # if isinstance(item_data, int):
+        product = get_object_or_404(Product, pk=item_id)
+        subtotal += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+            # otherwise, if the item data is a dictionary we know there are sizes
+        # else:
+        #     product = get_object_or_404(Product, pk=item_id)
+        #     for size, quantity in item_data['items_by_size'].items():
+        #         subtotal += quantity * product.price
+        #         product_count += quantity
+        #         bag_items.append({
+        #             'item_id': item_id,
+        #             'quantity': quantity,
+        #             'product': product,
+        #             'size': size,
+        #         })
+
+    if subtotal < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = subtotal * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - subtotal
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    total = delivery + subtotal
+
+    context = {
+        'bag_items': bag_items,
+        'subtotal': subtotal,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'total': total,
+    }
 
     return context
-    
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404)
+from products.models import Product
+from django.contrib import messages
 
 # Create your views here.
 def view_bag(request):
@@ -7,3 +10,26 @@ def view_bag(request):
     """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """
+    Add a quantity of the specified product to the shopping cart
+    """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    
+    # Session will persist while user is on that site
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+    else:
+        bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
