164e24cc9df3b6e245b0acbebc6fd5568888e661
Monica Murray <Monica.rickerby@gmail.com>
2023-06-10 12:46:33 +0000

    Add Sort feature, Search feature and Add to Cart buttons

--- products/views.py
+++ products/views.py
@@ -1,18 +1,75 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
 
 
 # Create your views here.
 
 def all_products(request):
-    """
-    A view to display all products in the store, including sorting
-    and search queries
-    """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    # If the Search function is used, search through all the products
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category_name':
+                sortkey = 'category_name__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+    # check if Category exists, split it into a list at the commas, use
+    # the list to filter the queryset of all products down to matching products
+        if 'category_name' in request.GET:
+            categories = request.GET['category_name'].split(',')
+            products = products.filter(category_name__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, 'You did not enter any search criteria')
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show a product's details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
