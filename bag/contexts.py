12d4ba599611efeb9384e0f0eec6fabac4df2c71
Monica Murray <Monica.rickerby@gmail.com>
2023-06-10 19:23:00 +0000

    Add context processor to bag app

---
+++ bag/contexts.py
@@ -0,0 +1,12 @@
+
+def bag_contents(request):
+    """
+    Instead of returning template, this function will return a dictionary
+    called 'context', it's purpose is to make the dictionary available
+    to all templates accross the entire application
+    """
+
+    context = {}
+
+    return context
+    
--- bookworms_et_al/settings.py
+++ bookworms_et_al/settings.py
@@ -80,6 +80,8 @@ TEMPLATES = [
                 'django.template.context_processors.request', #required by allauth, do not remove
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # make the context processor available accross the entire application
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -168,3 +170,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Calculate delivery costs
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
