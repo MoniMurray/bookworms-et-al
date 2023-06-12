153f9ac33ee8dd2766c3ea7d4a49ef1f813f6c5c
Monica Murray <Monica.rickerby@gmail.com>
2023-06-12 10:22:34 +0000

    Add admin view for Order, OrderLineItems

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+class OrderLineItemAdminInline(admin.TabularInline):
+    """ display the OrderLineItem model fields within the same page displaying the Order model fields"""
+
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+    
+
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Register the Order model for superuser's Admin use
+    """
+
+    inlines = (OrderLineItemAdminInline,)
+    # the following fields are calculated by model methods so read-only
+    readonly_fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+    )
+    
+    list_display = (
+        'order_number',
+        'date',
+        'full_name',
+        'order_total',
+        'delivery_cost',
+        'grand_total',
+    )
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
