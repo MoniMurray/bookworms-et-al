032ea5b6eceed02ce9541113eba776b965f50081
Monica Murray <Monica.rickerby@gmail.com>
2023-06-13 20:50:14 +0000

    Add profile template page, connect urls and view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Profile
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
+def profile (request):
+    """
+    Display the user's profile
+    """
+
+    template = 'profiles/profile.html'
+    context = {
+
+    }
+
+    return render(request, template, context)
+    
