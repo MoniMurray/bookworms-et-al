be1e2ed1ecb0a6ba4fabc2fccae3ccccebf9e4bb
Monica Murray <Monica.rickerby@gmail.com>
2023-06-15 21:13:33 +0000

    render the logged-in user's name to the profile template

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Profile
 from django.contrib.auth.decorators import login_required
 
@@ -9,9 +9,10 @@ def profile (request):
     """
     Display the user's profile
     """
-
+    profile = get_object_or_404(Profile, user=request.user)
     template = 'profiles/profile.html'
     context = {
+        'profile': profile,
 
     }
 
