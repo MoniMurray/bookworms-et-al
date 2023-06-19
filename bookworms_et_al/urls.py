2c4c117055e352f993a5b317a29e577ca19d82cb
Monica Murray <Monica.rickerby@gmail.com>
2023-06-19 10:55:26 +0000

    Add custom 404 error page

--- bookworms_et_al/urls.py
+++ bookworms_et_al/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 
 urlpatterns = [
@@ -29,3 +30,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'bookworms_et_al.views.handler404'
