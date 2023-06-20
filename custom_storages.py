367dfbab3ea104efdf7def985f1703dd614bbc8b
Monica Murray <Monica.rickerby@gmail.com>
2023-06-20 14:52:08 +0000

    Connect AWS S3 bucket to store static files

---
+++ custom_storages.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+# create custom class for static files
+class StaticStorage(S3Boto3Storage):
+    """
+    Custom class to upload and store product images
+    """
+
+    location = settings.STATICFILES_LOCATION
+
+# create custom class for MEDIA files
+class MediaStorage(S3Boto3Storage):
+
+    location = settings.MEDIAFILES_LOCATION
