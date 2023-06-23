026246520f2a390cd9deaa0ecbc73627d82d1a82
Monica Murray <Monica.rickerby@gmail.com>
2023-06-23 12:22:09 +0000

    Add Privacy Policy to main navigation

--- home/views.py
+++ home/views.py
@@ -15,3 +15,10 @@ def about_us(request):
     """
 
     return render(request, 'home/about_us.html')
+
+def privacy_policy(request):
+    """
+    A view to return the about_us page
+    """
+
+    return render(request, 'home/privacy_policy.html')
