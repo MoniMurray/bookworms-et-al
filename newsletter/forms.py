a728b5368075cd2ac91658878798572770e8330f
Monica Murray <Monica.rickerby@gmail.com>
2023-06-26 16:31:17 +0000

    Add the signup form for subscribing to newsletters

---
+++ newsletter/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Signup
+
+
+class SignupForm(forms.ModelForm):
+    """
+    A form to subscribe for newsletters
+    """
+
+    class Meta:
+        model = Signup
+        fields = ('name', 'email', 'subscribe',)
