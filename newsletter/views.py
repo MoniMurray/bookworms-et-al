662d613d030564ae86acf4b4f5ed57cca1ddaff6
Monica Murray <Monica.rickerby@gmail.com>
2023-06-26 20:19:26 +0000

    Add a signup template to newsletter app

--- newsletter/views.py
+++ newsletter/views.py
@@ -3,5 +3,25 @@ from .models import Signup
 
 # Create your views here.
 def signup(request):
-    "A view to create a user signup"
+    """
+    A view to create a user signup
+    """
+    
+    subscriber = []
+    template = 'newsletter/signup.html'
+
+    context = {}
+
+    # if request.method == 'POST':
+    #     email = request.POST.get('email', None)
+    #     name = request.POST.get('name', None)
+
+    #     if not email:
+    #         messages.error(request, 'Please enter a valid email address') 
+    #         return render('Signup/')
+
+    #     subscriber.save()
+    #     messages.success(request, 'You are successfully subscribed')
+
+    return render(request, template, context)
     
