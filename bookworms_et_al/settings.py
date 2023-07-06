e9bd09a3211a9265c04e84709041b8545db6a7c8
Monica Murray <Monica.rickerby@gmail.com>
2023-07-06 12:48:36 +0000

    Add order.country condition to checkout_success template and confirmation email body

--- bookworms_et_al/settings.py
+++ bookworms_et_al/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 # DEBUG = 'DEVELOPMENT' in os.environ
 # if 'DATABASE_URL' in os.environ:
 #     DATABASES = {
