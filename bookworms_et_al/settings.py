f42320582df3d0bc7a7ef68ada52674022496ae8
Monica Murray <Monica.rickerby@gmail.com>
2023-07-09 12:17:09 +0000

    Change Debug to False, remove commented out code

--- bookworms_et_al/settings.py
+++ bookworms_et_al/settings.py
@@ -28,19 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = 'DEVELOPMENT' in os.environ
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
+# DEBUG = True
+
 
 
 ALLOWED_HOSTS = [
@@ -152,7 +141,6 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-    # print("Connected to Elephant SQL")
 else:
     DATABASES = {
         'default': {
@@ -160,7 +148,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-    # print("Connected to Local DB")
 
 
 # Password validation
